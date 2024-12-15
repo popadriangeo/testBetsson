fa7f5b49006c2e2a62c0c3457ef6b60e3733c803
Adrian Pop <adrian.pop@sag-digital.de>
2024-12-15 17:13:12 +0200

    added Verification method for deposit endpoint

--- qa-backend-code-challenge/tests/Betsson.OnlineWallets.Api.Tests/Tests/OnlineWalletApiTests.cs
+++ qa-backend-code-challenge/tests/Betsson.OnlineWallets.Api.Tests/Tests/OnlineWalletApiTests.cs
@@ -26,9 +26,26 @@ namespace Betsson.OnlineWallets.Api.Tests.Tests
             response.Data.Should().NotBeNull();
             response.Data.Amount.Should().BeGreaterOrEqualTo(0);
         }
+
+        [Fact]
+        public async Task Deposit_ShouldReturnOk()
+        {
+            var request = new RestRequest("/onlinewallet/deposit", Method.Post);
+            request.AddJsonBody(new DepositRequest { Amount = 50.0 });
+            var response = await _client.ExecuteAsync<BalanceResponse>(request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Data.Should().NotBeNull();
+        }
+
     }
 
     public class BalanceResponse
+    {
+        public double Amount { get; set; }
+    }
+
+    public class DepositRequest
     {
         public double Amount { get; set; }
     }
